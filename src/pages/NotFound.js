a4b1776f6fec7791c513bd7368e009e6f7fd2a5e
ZetRC <zetrosas555@gmail.com>
2022-03-14 12:28:22 -0600

    changes in not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function NotFound() {
   return (
     <div className="custom-container">
       <h1 className="text-3xl">404</h1>
       <div>La página no fué encontrada</div>
+      <Link className=' text-mainGreen' to="/">Volver a home</Link>
     </div>
   )
 }
